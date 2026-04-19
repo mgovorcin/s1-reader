6b78188d27920c3df6bd5a7b65c10d9763a4054d
mgovorcin <marin.govorcin@jpl.nasa.gov>
2026-04-19 12:11:09 -0700

    Export load_bursts_from_zarr from s1reader package

--- src/s1reader/__init__.py
+++ src/s1reader/__init__.py
@@ -4,4 +4,5 @@ from s1reader.version import release_version as __version__
 # top-level functions to be easily used
 from s1reader.s1_burst_slc import Sentinel1BurstSlc
 from s1reader.s1_reader import load_bursts
+from s1reader.s1_reader_zarr import load_bursts_from_zarr
 from s1reader.s1_orbit import get_orbit_file_from_dir
